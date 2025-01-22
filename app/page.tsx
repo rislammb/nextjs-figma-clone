0ce834589d40ca14a21ee3d431693caffc27e967
Rayhanul Islam <rislammb@gmail.com>
2025-01-22 14:56:31 +0600

    started to show active users avatar

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,15 @@
 "use client";
 
 import Live from "@/components/Live";
+import Navbar from "@/components/Navbar";
 
 export default function Home() {
   return (
-    <div>
-      <Live />
+    <div className="h-screen overflow-hidden">
+      <Navbar />
+      <section className="flex h-full flex-row">
+        <Live />
+      </section>
     </div>
   );
 }
