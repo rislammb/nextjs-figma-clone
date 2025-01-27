1831b89be6c9e518078ea081696a913f595a3b9a
Rayhanul Islam <rislammb@gmail.com>
2025-01-27 12:46:23 +0600

    added modify text content

--- components/RightSidebar.tsx
+++ components/RightSidebar.tsx
@@ -6,7 +6,14 @@ import Export from "./settings/Export";
 import Text from "./settings/Text";
 import { modifyShape } from "@/lib/shapes";
 
-export default function RightSidebar({ elementAttributes, setElementAttributes, fabricRef, isEditingRef, activeObjectRef, syncShapeInStorage }: RightSidebarProps) {
+export default function RightSidebar({
+  elementAttributes,
+  setElementAttributes,
+  fabricRef,
+  isEditingRef,
+  activeObjectRef,
+  syncShapeInStorage,
+}: RightSidebarProps) {
   const handleInputChange = (property: string, value: string) => {
     if (!isEditingRef.current) isEditingRef.current = true;
 
@@ -24,10 +31,22 @@ export default function RightSidebar({ elementAttributes, setElementAttributes, 
   return (
     <section className="flex flex-col border-t border-primary-grey-200 bg-primary-black text-primary-grey-300 min-w-[227px] sticky right-0 h-full max-sm:hidden select-none">
       <h3 className="px-5 pt-4 text-xs uppercase">Design</h3>
-      <span className="text-xs text-primary-grey-300 mt-3 px-5 border-b border-primary-grey-200 pb-4">Make changes to canvas as you like</span>
+      <span className="text-xs text-primary-grey-300 mt-3 px-5 border-b border-primary-grey-200 pb-4">
+        Make changes to canvas as you like
+      </span>
 
-      <Dimensions width={elementAttributes.width} height={elementAttributes.height} isEditingRef={isEditingRef} handleInputChange={handleInputChange} />
-      <Text />
+      <Dimensions
+        width={elementAttributes.width}
+        height={elementAttributes.height}
+        isEditingRef={isEditingRef}
+        handleInputChange={handleInputChange}
+      />
+      <Text
+        fontFamily={elementAttributes.fontFamily}
+        fontSize={elementAttributes.fontSize}
+        fontWeight={elementAttributes.fontWeight}
+        handleInputChange={handleInputChange}
+      />
       <Color />
       <Color />
       <Export />
